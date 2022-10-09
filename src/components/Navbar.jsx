5ded9eab90d9d1fa4cded0355b604ecfb39f7352
Jefferson Leonata <leonatajeff@gmail.com>
2022-10-09 15:19:56 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <nav class="p-2 mt-0 fixed w-full z-10 bottom-0">
-            <div class="absolute text-white bottom-0 right-0 space-x-8 p-6 text-lg bg-white-500/50 backdrop-blur-sm bg-contain">
+        <nav class="flex justify-center mt-0 fixed w-full z-10 bottom-0 backdrop-filter backdrop-blur-lg">
+            <div class="lg:absolute text-white bottom-0 right-0 space-x-8 p-6 text-lg">
                 <NavLink to='/'>Join Us</NavLink>
                 <NavLink to='/mission'>Our Mission</NavLink>
                 <NavLink to='/team'>Our Team</NavLink>
